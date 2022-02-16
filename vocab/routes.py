00f0d90bb8af48e38b886eaf050569c11090b4d3
ak-fishbarc <rybarczyk.ak@gmail.com>
2022-02-16 23:14:27 +0000

    Project III

---
+++ vocab/routes.py
@@ -0,0 +1,51 @@
+from vocab import app
+from vocab import db
+from vocab.models import User
+from flask import flash, render_template, request, redirect, url_for
+from vocab.forms import LoginForm, RegistrationForm
+from flask_login import current_user, login_required, login_user, logout_user
+from werkzeug.urls import url_parse
+
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
+@login_required
+def index():
+    return render_template('index.html', title='Learn Vocabulary')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(url_for('index'))
+    return render_template('login.html', title="Sign In", form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/registration', methods=['GET', 'POST'])
+def registration():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('registration.html', title='Register', form=form)
